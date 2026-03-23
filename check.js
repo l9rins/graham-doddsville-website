2a5255cf40b56fe621ad6539a387cf57bc5cf3bf
Marquee <lorenzbarangan112@gmail.com>
2026-03-24 02:35:02 +0800

    Apply all outstanding client fixes: economics/events titles, placeholder cleanup, Greatest Investors, resources, nav fixes, junk file removal

--- check.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const data = fs.readFileSync('books_clean.json');
-
-console.log('Bytes:', data.slice(0,10));
-
-console.log('String:', data.toString('utf8').slice(0,10));
