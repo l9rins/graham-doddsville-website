69efe1a2cba413c8002422a607e2b30964ab11eb
Marquee <lorenzbarangan112@gmail.com>
2026-05-04 09:24:59 +0800

    Standardize book pages to unified premium mobile design and migrate to public directory for serving

--- server.js
+++ server.js
@@ -842,7 +842,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Error handling
